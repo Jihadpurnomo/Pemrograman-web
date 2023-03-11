ad3458549ce29b39ebe532d4725860e709ed07a9
Jihadpurnomo <90895465+Jihadpurnomo@users.noreply.github.com>
2023-03-11 18:08:53 +0700

    Add files via upload

---
+++ PHP/pertemuan 3/tugas2.php
@@ -0,0 +1,15 @@
+<?php
+
+$nilai1 = 10;
+$nilai2 = 7;
+$bobotN1 = 2;
+$bobotN2 = 8;
+$hasilAkhir = ($nilai1 * $bobotN1) + ($nilai2 * $bobotN2);
+$hasilBobot = $nilai1 + $nilai2;
+
+echo "total Nilai = {$hasilAkhir} <br>";
+echo "hasil dari 10 + 7 = {$hasilBobot} <br> <br>";
+echo "Nama: Jihad Poernomo <br>";
+echo "NIM: 121103007";
+
+?>
